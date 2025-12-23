acab161ee28840ca9f0d8666664e4faffef03fda
Bubbley <sehee4329@gmail.com>
2025-12-23 17:50:02 +0900

    deploy : 배포
    - 작은 오류(오타) 수정
    - 배포 보안용 파일 (robots, vercel) 추가

--- src/pages/Certificate.jsx
+++ src/pages/Certificate.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from "react"
 import Footer from "../components/Footer"
+import Banner from "../components/certificate/Banner"
 import Board from "../components/certificate/Board"
 import Introduce from "../components/certificate/Introduce"
 import Menu from "../components/certificate/Menu"
-import Banner from "../components/certificate/banner"
 
 const Certificate = () => {
 	useEffect(() => {
